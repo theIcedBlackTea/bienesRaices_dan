d395df3885cf14f166c0126628d0ac23fa431f90
theIcedBlackTea <blacktemplehood@gmail.com>
2024-12-12 04:03:46 -0600

    Actividad 2, Implementando paleta de colores y diseño a los componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./views/**/*.pug'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        negro: '#000000',
+        blanco: '#FFFFFF',
+        naranja: '#BC6C25',
+        verde: '#606C38',
+        beige: '#FFFEF0',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
